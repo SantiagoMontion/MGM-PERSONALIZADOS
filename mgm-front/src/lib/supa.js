eb2314b3e155cb693817eee985377ffdbf528a1e
Montion <82060780+SantiagoMontion@users.noreply.github.com>
2025-10-09 02:18:38 -0300

    Use Supabase SDK signed uploads

--- mgm-front/src/lib/supa.js
+++ mgm-front/src/lib/supa.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-
-export const supa = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supa = createClient(url, key);
